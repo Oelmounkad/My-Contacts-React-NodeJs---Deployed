4beda9a502918ffff3f8febfc539f9324016e80c
Elmounkad <oelmounkad@gmail.com>
2020-07-14 19:36:38 +0200

    finished authentication and jwt token header

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,20 @@ const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator');
 const config = require('config')
+const auth = require('../middleware/auth')
 
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/',(req,res) => {
-    res.send('Get a logged in user')
+router.get('/',auth, async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json({user})
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error')
+    }
 })
 
 
